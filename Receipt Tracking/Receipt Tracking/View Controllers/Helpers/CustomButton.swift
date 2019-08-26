35c88081c4f291e1eb0428cad7e384875af57f74
Jake <jlconnerly@gmail.com>
2019-08-26 11:58:47 -0500

    Added Helpers File

---
+++ Receipt Tracking/Receipt Tracking/View Controllers/Helpers/CustomButton.swift
@@ -0,0 +1,16 @@
+//
+//  CustomButton.swift
+//  Receipt Tracking
+//
+//  Created by Jake Connerly on 8/26/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomButton: UIButton {
+    
+
+    
+}
