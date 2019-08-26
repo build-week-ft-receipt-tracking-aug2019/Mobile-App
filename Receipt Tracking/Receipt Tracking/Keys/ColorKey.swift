bb2a3205039f0f1a76076dc1a16949d1d7882bae
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-08-26 11:54:18 -0800

    UI Login laid out and constraints set in storyboard

---
+++ Receipt Tracking/Receipt Tracking/Keys/ColorKey.swift
@@ -0,0 +1,18 @@
+//
+//  ColorKey.swift
+//  Receipt Tracking
+//
+//  Created by Alex Rhodes on 8/26/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var receiptWhite = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+    static var receiptDarkGreen = UIColor(red:0.00, green:0.23, blue:0.21, alpha:1.0)
+    static var receiptGray = UIColor(red:0.90, green:0.91, blue:0.90, alpha:1.0)
+    static var receiptLightGreen = UIColor(red:0.36, green:0.73, blue:0.44, alpha:1.0)
+    static var receiptMidGreen = UIColor(red:0.14, green:0.59, blue:0.20, alpha:1.0)
+}
--- Receipt Tracking/Receipt Tracking/View Controllers/LoginViewController.swift
+++ Receipt Tracking/Receipt Tracking/View Controllers/LoginViewController.swift
@@ -5,15 +5,34 @@
 //  Created by Jake Connerly on 8/26/19.
 //  Copyright © 2019 jake connerly. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    private func setViews() {
+        view.backgroundColor = .receiptGray
+        
+    }
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
     }
     
 
