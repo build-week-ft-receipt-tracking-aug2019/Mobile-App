11fb54c6ded2c0bf6c1274f455719d1453ce6535
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-08-27 18:17:39 -0800

    EditRecieptVC and EditRecieptStoryboard complete

--- Receipt Tracking/Receipt Tracking/Keys/AddView.swift
+++ Receipt Tracking/Receipt Tracking/Keys/AddView.swift
@@ -11,50 +11,36 @@ import UIKit
 
 class AddView {
     
-    func datePickerConfiguration(picker: UIDatePicker) {
-        picker.datePickerMode = .date
-    }
-    
-    func categoryPickerConfiguration(picker: UIPickerView, textField: UITextField) {
-        
-        picker.backgroundColor = .black
-        picker.setValue(UIColor.receiptWhite, forKey: "textColor")
-        
-        let toolBar = UIToolbar()
-        toolBar.barStyle = UIBarStyle.default
-        toolBar.isTranslucent = true
-        toolBar.tintColor = .black
-        toolBar.sizeToFit()
-        
-        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: Selector(("donePicker")))
-        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: Selector(("donePicker")))
-        
-        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-        toolBar.isUserInteractionEnabled = true
-
-        textField.inputView = picker
-        textField.inputAccessoryView = toolBar
-        
-//        picker.addSubview(toolBar)
-        
-    }
-    
     func viewConfiguration(view: UIView) {
+        
         // Set background color
         view.backgroundColor = .black
+        
     }
     
     func navBarConfiguration(navBar: UINavigationBar) {
-        // Setting navigation bar to a custom color
         
+        // Setting navigation bar to black
         navBar.barStyle = .black
-//        navBar.barTintColor = .receiptMidGreen
-//        navBar.backgroundColor = .receiptMidGreen
-//        navBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.receiptWhite]
+        
+    }
+    
+    func editUploadedPhoto(button: UIButton) {
+        
+        // Set background color
+        button.backgroundColor = .receiptMidGreen
+        
+        // Set title name
+        button.setTitle("EDIT RECEIPT IMAGE", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        // Set corner radius
+        button.layer.cornerRadius = 5
+        
     }
     
     func photoUploadButtonConfiguration(button: UIButton) {
+        
         // Set background color
         button.backgroundColor = .receiptMidGreen
         
@@ -64,9 +50,11 @@ class AddView {
         
         // Set corner radius
         button.layer.cornerRadius = 5
+        
     }
     
     func textFieldConfiguration(textField: UITextField) {
+        
         // Set background color
         textField.backgroundColor = .receiptGray
         
@@ -79,23 +67,24 @@ class AddView {
     }
     
     func imageViewConfiguration(imageView: UIImageView) {
+        
         // Set background color
         imageView.backgroundColor = .darkGray
+        
     }
     
-//    func datePickerConfiguration(picker: UIDatePicker) {
-//        // Set text color
-//        picker.setValue(UIColor.receiptWhite, forKey: "textColor")
-//    }
     
     func barButtonItemConfiguration(barButton: UIBarButtonItem) {
+        
         // Set text color
         barButton.tintColor = .receiptWhite
+        
     
     }
     
     func toolbarConfiguration(toolbar: UIToolbar) {
+        
         toolbar.barTintColor = .black
+        
     }
-    
 }
