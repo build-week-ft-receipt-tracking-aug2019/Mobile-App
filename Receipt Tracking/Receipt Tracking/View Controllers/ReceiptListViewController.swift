7f07598908c1afed16594880d2dfb3b137bf2f8b
Percy <percival.ngan@gmail.com>
2019-08-28 14:51:35 -0500

    Formatted navigationBar in ReceiptList view

--- Receipt Tracking/Receipt Tracking/View Controllers/ReceiptListViewController.swift
+++ Receipt Tracking/Receipt Tracking/View Controllers/ReceiptListViewController.swift
@@ -15,7 +15,8 @@ class ReceiptListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    let receiptController = ReceiptController.shared
+	var viewDetails = AddView()
+	let receiptController = ReceiptController.shared
     var receipts: [ReceiptRepresentation] = []
     var user: UserRepresentation {
         let moc = CoreDataStack.shared.mainContext
@@ -69,6 +70,10 @@ class ReceiptListViewController: UIViewController {
         } else if user.username == nil {
             performSegue(withIdentifier: "LoginViewModalSegue", sender: self)
         }
+
+		// Change the background color of the table view
+		setupViews()
+		self.tableView.backgroundColor = UIColor.lightGray
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -84,6 +89,18 @@ class ReceiptListViewController: UIViewController {
             }
         }
     }
+
+
+	private func setupViews() {
+
+		// Formats navigation bar and view background
+		viewDetails.navBarConfiguration2(navBar: navigationController!.navigationBar)
+		viewDetails.viewConfiguration(view: view)
+		viewDetails.navBarConfiguration(navBar: navigationController!.navigationBar)
+
+
+
+	}
     
     
 
